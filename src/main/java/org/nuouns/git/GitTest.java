b3945ea5d15db8892deff32d7226e46310659cc8
shepherd <shepherd_joe@163.com>
2022-09-15 21:36:37 +0800

    push test

--- src/main/java/org/nuouns/git/GitTest.java
+++ src/main/java/org/nuouns/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git hot-fix");
         System.out.println("hello git hot-fix test");
         System.out.println("hello git hot-fix merge");
+        System.out.println("push test");
     }
 }
